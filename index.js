aec672ea9024c235ec7ffff164ffe9f5ec51577f
Sowwiezn <businka16.04.2007@gmail.com>
2023-10-26 17:52:43 +0300

    v15

--- index.js
+++ index.js
@@ -20,26 +20,26 @@ server.listen(3000)
 function getAllJokes(req, res){
     let dir = fs.readdirSync(dataPath);
     let allJokes = [];
-    // for(let i = 0; i < dir.length; i++) {
-    //     let file = fs.readFileSync(path.join(dataPath, i+'.json'));
-    //     let jokeJson = Buffer.from(file).toString();
-    //     let joke = JSON.parse(jokeJson);
-    //     joke.id = i;
-
-    //     allJokes.push(joke);
-    // }
     for(let i = 0; i < dir.length; i++) {
-        try {
-            let file = fs.readFileSync(path.join(dataPath, dir[i]));
-            let jokeJson = Buffer.from(file).toString();
-            let joke = JSON.parse(jokeJson);
-            joke.id = i;
-    
-            allJokes.push(joke);
-        } catch (error) {
-            console.error(`Ошибка при чтении файла ${dir[i]}: ${error.message}`);
-        }
+        let file = fs.readFileSync(path.join(dataPath, i+'.json'));
+        let jokeJson = Buffer.from(file).toString();
+        let joke = JSON.parse(jokeJson);
+        joke.id = i;
+
+        allJokes.push(joke);
     }
+    // for(let i = 0; i < dir.length; i++) {
+    //     try {
+    //         let file = fs.readFileSync(path.join(dataPath, dir[i]));
+    //         let jokeJson = Buffer.from(file).toString();
+    //         let joke = JSON.parse(jokeJson);
+    //         joke.id = i;
+    
+    //         allJokes.push(joke);
+    //     } catch (error) {
+    //         console.error(`Ошибка при чтении файла ${dir[i]}: ${error.message}`);
+    //     }
+    // }
     res.end(JSON.stringify(allJokes));
 }
 
